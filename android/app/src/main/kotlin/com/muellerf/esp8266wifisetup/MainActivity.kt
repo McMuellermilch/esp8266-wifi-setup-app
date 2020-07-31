acfe33853c5e2732481bc017471395c79cb8836e
Florian <mueller.florian99@outlook.com>
2020-08-01 01:02:08 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/muellerf/esp8266wifisetup/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.muellerf.esp8266wifisetup
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,18 @@
+import 'package:esptouch_flutter/esptouch_flutter.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ESP8266 WIFI SETUP"),
+      ),
+    );
+  }
+}
